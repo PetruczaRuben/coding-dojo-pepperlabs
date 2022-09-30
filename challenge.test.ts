42f339ae478f8b1f06930e7adc43bfa879baeb24
Ruben Petrucza <ruben.petrucza@pepperlabs.io>
2022-09-30 14:56:05 +0200

    Update challenge to snail

--- challenge.test.ts
+++ challenge.test.ts
@@ -1,7 +1,104 @@
-import {highestScoringWord} from './challenge';
+import {snail, SnailIterator} from './challenge';
 
-describe('highestScoringWord', () => {
-  test('dummy test', () => {
-    expect(highestScoringWord('Lorem ipsum dolor sit amet')).toEqual(' ');
+describe.skip('snail', () => {
+  test('works with 2x2 matrix', () => {
+    expect(snail([
+      [1, 2],
+      [3, 4]
+    ])).toEqual([1, 2, 4, 3]);
+
+    expect(snail([
+      [2, 3],
+      [4, 5]
+    ])).toEqual([2, 3, 5, 4]);
+  });
+
+  test('works with 3x3 matrix', () => {
+    expect(snail([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ])).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
   });
 });
+
+describe('SnailIterator', () => {
+  test('returns the upper left element first', () => {
+    const subject = new SnailIterator([
+      [1, 2],
+      [3, 4]
+    ]);
+
+    expect(subject.next()).toEqual(1);
+  });
+
+  test('returns the top right element for the second time', () => {
+    const subject = new SnailIterator([
+      [1, 2],
+      [3, 4]
+    ]);
+
+    subject.next();
+    expect(subject.next()).toEqual(2);
+  });
+
+  test('returns the last element of the second array for the third time', () => {
+    const subject = new SnailIterator([
+      [1, 2],
+      [3, 4]
+    ]);
+
+    subject.next();
+    subject.next();
+    expect(subject.next()).toEqual(4);
+  });
+
+  test('returns the first element of the second array for the fourth time', () => {
+    const subject = new SnailIterator([
+      [1, 2],
+      [3, 4]
+    ]);
+
+    subject.next();
+    subject.next();
+    subject.next();
+    expect(subject.next()).toEqual(3);
+  });
+
+  test('returns the last element of the first array for the third time', () => {
+    const subject = new SnailIterator([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]);
+
+    subject.next();
+    subject.next();
+    expect(subject.next()).toEqual(3);
+  });
+
+  test('returns the first element of the third array for the 7th time', () => {
+    const subject = new SnailIterator([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]);
+
+    expect(subject.next()).toEqual(1);
+    expect(subject.next()).toEqual(2);
+    expect(subject.next()).toEqual(3);
+    expect(subject.next()).toEqual(6);
+    expect(subject.next()).toEqual(9);
+    expect(subject.next()).toEqual(8);
+    expect(subject.next()).toEqual(7);
+    expect(subject.next()).toEqual(4);
+  });
+});
+
+/*
+Ruben
+Sanyo
+Balazs
+Dobi
+Tomi
+ */
--- challenge.ts
+++ challenge.ts
@@ -1,3 +1,66 @@
-export const highestScoringWord = (words: string): string => {
-  return '';
+export const snail = (matrix: number[][]): number[] => {
+  const iter = new SnailIterator(matrix);
+  const result: number[] = [];
+
+  while (true) {
+    const next = iter.next();
+    if (!next) return result;
+    result.push(next);
+  }
 };
+
+export class SnailIterator {
+  private readonly size: number;
+  private x = -1;
+  private y = 0;
+
+  constructor(private readonly matrix: number[][]) {
+    this.size = matrix.length - 1;
+  }
+
+  next(): number | null {
+    if (this.cannotGoLeftAnymore() && !this.cannotGoUpAnymore()) {
+      this.stepUp();
+    } else if (this.cannotGoDownAnymore()) {
+      this.stepLeft();
+    } else if (this.cannotGoRightAnymore()) {
+      this.stepDown();
+    } else if (this.cannotGoUpAnymore()) {
+      this.stepRight();
+    }
+
+    return this.matrix[this.y][this.x]
+  }
+
+  private stepDown() {
+    this.y++;
+  }
+
+  private stepUp() {
+    this.y--;
+  }
+
+  private stepRight() {
+    this.x++;
+  }
+
+  private stepLeft() {
+    this.x--;
+  }
+
+  private cannotGoRightAnymore() {
+    return this.x === this.size;
+  }
+
+  private cannotGoDownAnymore() {
+    return this.y === this.size;
+  }
+
+  private cannotGoLeftAnymore() {
+    return this.x === 0;
+  }
+
+  private cannotGoUpAnymore() {
+    return this.y === 0;
+  }
+}
