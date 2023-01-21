6ffb1547b53a067f1e203e9f05ef9479a574750f
Ruben Petrucza <ruben.petrucza@pepperlabs.io>
2023-01-21 12:07:04 +0100

    Add test cases

--- challenge.test.ts
+++ challenge.test.ts
@@ -1,12 +1,33 @@
 import {twoSum} from './challenge';
 
-// Sanyo
-// Balazs
-// Dobi
-// Ruben
+describe('#twoSum', () => {
+  test('returns the only possible solution in case of two-element array', () => {
+    expect(twoSum([0, 0], 0).sort()).toEqual([0, 1]);
 
-describe('#extractRanges', () => {
-  test('example test case', () => {
-    expect(twoSum([1, 2, 3], 3)).toEqual([0, 1]);
+    const numbers = [0, 0];
+    const target = 0;
+    const solution = twoSum(numbers, target);
+    expect(solution.reduce((sum, index) => sum + numbers[index]!, 0)).toEqual(target);
+  });
+
+  test('works for three-element array', () => {
+    expect(twoSum([0, 1, 2], 3).sort()).toEqual([1, 2]);
+  });
+
+  test('works for four-element array', () => {
+    expect(twoSum([0, 1, 2, 3], 5).sort()).toEqual([2, 3]);
+  });
+
+  test('works for every four element array', () => {
+    expect(twoSum([0, 1, 2, 3], 3).sort()).toEqual([1, 2]);
+  });
+
+  test.skip("doesn't use the same element twice and it's still gives a valid solution", () => {
+    const numbers = [0, 1, 0];
+    const target = 0;
+    const solution = twoSum(numbers, target);
+    expect(solution.reduce((sum, index) => sum + numbers[index]!, 0)).toEqual(target);
+
+    expect(solution.sort()).not.toEqual([0, 0]);
   });
 });
